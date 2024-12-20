3eb40f3db3b52a27ddab0cc861b92508e04bec5d
Jesse Vaerendonck <jesse.vaerendonck@student.hogent.be>
2024-12-20 17:52:35 +0100

    fix undo cascades

---
+++ backend/src/data/migrations/20241220165226_undo_cascades/migration.sql
@@ -0,0 +1,11 @@
+-- DropForeignKey
+ALTER TABLE `game` DROP FOREIGN KEY `Game_round_id_fkey`;
+
+-- DropForeignKey
+ALTER TABLE `round` DROP FOREIGN KEY `Round_tournament_id_fkey`;
+
+-- AddForeignKey
+ALTER TABLE `Round` ADD CONSTRAINT `Round_tournament_id_fkey` FOREIGN KEY (`tournament_id`) REFERENCES `Tournament`(`tournament_id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `Game` ADD CONSTRAINT `Game_round_id_fkey` FOREIGN KEY (`round_id`) REFERENCES `Round`(`round_id`) ON DELETE CASCADE ON UPDATE CASCADE;
