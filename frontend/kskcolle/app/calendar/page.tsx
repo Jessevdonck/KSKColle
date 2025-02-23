4ba76c1a24baf155a142db0fc195af5b8851b898
Jesse Vaerendonck <jesse.vaerendonck@student.hogent.be>
2025-02-23 11:38:58 +0100

    mockdata calendar events verwijderd

--- frontend/kskcolle/app/calendar/page.tsx
+++ frontend/kskcolle/app/calendar/page.tsx
@@ -1,10 +1,9 @@
 import Calendar from './components/Calendar'
-import { calendarEvents } from '../../data/mock_data'
 
 export default function CalendarPage() {
   return (
     <div className="bg-neutral-50 min-h-screen">
-      <Calendar events={calendarEvents} />
+      <Calendar />
     </div>
   )
 }
