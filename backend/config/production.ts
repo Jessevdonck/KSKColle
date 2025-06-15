ca1997e507013148e815de2813b026d0f4c6ac94
Jesse Vaerendonck <jesse.vaerendonck@student.hogent.be>
2025-06-15 12:26:20 +0200

    backend test

--- backend/config/production.ts
+++ backend/config/production.ts
@@ -7,7 +7,7 @@ export default {
   },
   cors: {
     // 👈 1
-    origins: ['https://kskcolle-production.up.railway.app/'], // 👈 2
+    origins: ['https://frontendweb-kskcolle.onrender.com'], // 👈 2
     maxAge: 3 * 60 * 60, // 👈 3
   },
   auth: {
