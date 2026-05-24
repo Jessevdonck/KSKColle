3ad7035667a96621af63e63ab80d13c4b8dce866
Jesse Vaerendonck <jesse.vaerendonck@student.hogent.be>
2026-05-25 00:09:41 +0200

    fix

---
+++ frontend/kskcolle/eslint.config.mjs
@@ -0,0 +1,28 @@
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+import { FlatCompat } from "@eslint/eslintrc";
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+});
+
+const eslintConfig = [
+  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    rules: {
+      "@typescript-eslint/no-unused-vars": [
+        "warn",
+        {
+          vars: "all",
+          args: "after-used",
+          ignoreRestSiblings: true,
+          argsIgnorePattern: "^_",
+        },
+      ],
+    },
+  },
+];
+
+export default eslintConfig;
